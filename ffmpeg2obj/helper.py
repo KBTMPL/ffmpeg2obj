6ea8b20c7b79a0943a27dd49b6d2088b05110685
Krzysztof Bulanda <ictkontakt@gmail.com>
2024-02-27 22:57:47 +0100

    feat: add string representation

--- ffmpeg2obj/helper.py
+++ ffmpeg2obj/helper.py
@@ -12,3 +12,12 @@ class ProcessedFile:
         self.is_locked = is_locked
         self.is_uploaded = is_uploaded
         self.hashed_name: int = hash(object_name)
+
+    def __str__(self) -> str:
+        out = []
+        out += ["object_name: " + self.object_name]
+        out += ["real_path: " + self.real_path]
+        out += ["is_locked: " + str(self.is_locked)]
+        out += ["is_uploaded: " + str(self.is_uploaded)]
+        out += ["hashed_name: " + str(self.hashed_name)]
+        return "\n".join(out)
