a4051a3183909dfb9c824db0369a7eb207125690
Krzysztof Bulanda <ictkontakt@gmail.com>
2024-02-27 01:14:12 +0100

    chore: add hashed name attr to ProcessedFile class

--- ffmpeg2obj/helper.py
+++ ffmpeg2obj/helper.py
@@ -5,13 +5,13 @@ class ProcessedFile:
     """Class to describe processed files"""
     def __init__(
         self,
-        object_name,
-        real_path,
-        is_locked,
-        is_uploaded
-        
+        object_name: str,
+        real_path: str,
+        is_locked: bool,
+        is_uploaded: bool
     ) -> None:
         self.object_name = object_name
         self.real_path = real_path
         self.is_locked = is_locked
         self.is_uploaded = is_uploaded
+        self.hashed_name: int = hash(object_name)
