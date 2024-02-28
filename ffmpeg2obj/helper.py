ee7c948f1a0af836174d37884eeda4946c1d3a28
Krzysztof Bulanda <ictkontakt@gmail.com>
2024-02-28 20:25:30 +0100

    feat: add method that updates ProcessedFile object attrs

--- ffmpeg2obj/helper.py
+++ ffmpeg2obj/helper.py
@@ -1,5 +1,8 @@
 """Module with helper classes for ffmpeg2obj"""
 
+import boto3
+import botocore
+
 
 class ProcessedFile:
     """Class to describe processed files"""
@@ -12,6 +15,7 @@ class ProcessedFile:
         self.is_locked = is_locked
         self.is_uploaded = is_uploaded
         self.hashed_name: int = hash(object_name)
+        self.object_lock_file_name = self.object_name + ".lock"
 
     def __str__(self) -> str:
         out = []
@@ -21,3 +25,24 @@ class ProcessedFile:
         out += ["is_uploaded: " + str(self.is_uploaded)]
         out += ["hashed_name: " + str(self.hashed_name)]
         return "\n".join(out)
+
+    def update(self, obj_config: dict, bucket_name: str) -> None:
+        """Updates ProcessedFile object instance attributes"""
+        self.is_locked = file_exists(
+            self.object_lock_file_name, obj_config, bucket_name
+        )
+        self.is_uploaded = file_exists(
+            self.object_name, obj_config, bucket_name
+        )
+
+
+def file_exists(file: str, obj_config: dict, bucket_name: str) -> bool:
+    """Checks if given file exists in requested bucket"""
+    obj_client = boto3.client("s3", **obj_config)
+    try:
+        obj_client.head_object(Bucket=bucket_name, Key=file)
+        return True
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            return False
+        return False
