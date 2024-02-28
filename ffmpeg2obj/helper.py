b27bf4bd29e20436a4fe128369a78b05e6a7211c
Krzysztof Bulanda <ictkontakt@gmail.com>
2024-02-28 21:44:08 +0100

    feat: ground work for file uploads

--- ffmpeg2obj/helper.py
+++ ffmpeg2obj/helper.py
@@ -14,7 +14,9 @@ class ProcessedFile:
         self.real_path = real_path
         self.is_locked = is_locked
         self.is_uploaded = is_uploaded
-        self.hashed_name: int = hash(object_name)
+        hasher = hashlib.sha256()
+        hasher.update(self.object_name.encode("utf8"))
+        self.hashed_name: str = hasher.hexdigest()
         self.object_lock_file_name = self.object_name + ".lock"
 
     def __str__(self) -> str:
@@ -23,7 +25,7 @@ class ProcessedFile:
         out += ["real_path: " + self.real_path]
         out += ["is_locked: " + str(self.is_locked)]
         out += ["is_uploaded: " + str(self.is_uploaded)]
-        out += ["hashed_name: " + str(self.hashed_name)]
+        out += ["hashed_name: " + self.hashed_name]
         return "\n".join(out)
 
     def update(self, obj_config: dict, bucket_name: str) -> None:
@@ -36,6 +38,16 @@ class ProcessedFile:
             self.is_locked = lock_file_exist
         if uploaded_file_exist is not None:
             self.is_uploaded = uploaded_file_exist
+
+    def upload(self, obj_config: dict, bucket_name: str) -> bool:
+        """Uploads converted file from /tmp to object storage bucket"""
+        obj_client = boto3.client("s3", **obj_config)
+        try:
+            obj_client.upload_file(self.hashed_name, bucket_name, self.object_name)
+        except botocore.exceptions.ClientError as e:
+            print(e)
+            return False
+        return True
 
 
 def file_exists(file: str, obj_config: dict, bucket_name: str) -> bool | None:
